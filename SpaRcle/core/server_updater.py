7765be931066b34f0d51940e0a622e5bac649201
Monika0000 <rotaru5craft@gmail.com>
2024-06-28 22:10:15 +0100

    (Server) Commit server_updater.py

---
+++ SpaRcle/core/server_updater.py
@@ -0,0 +1,32 @@
+from termcolor import colored
+import git
+import time
+import os
+import threading
+
+
+def pull_changes():
+    repo = git.Repo(os.getcwd())
+    origin = repo.remotes.origin
+    origin.fetch()
+
+    # repo.is_dirty(untracked_files=True) or
+    if repo.head.commit != origin.refs.master.commit:
+        print(colored('Server repository has changes, reload...', 'yellow'))
+        origin.pull()
+        restart_server()
+
+
+def restart_server():
+    print(colored('Restarting server...', 'yellow'))
+    exit(100)
+
+
+def changes_checker():
+    while True:
+        pull_changes()
+        time.sleep(1)
+
+
+def run_server_updater():
+    threading.Thread(target=changes_checker, daemon=True).start()
