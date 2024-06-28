eb3bb5cea061645fb211ec5562fca0c54eb68a88
Monika0000 <rotaru5craft@gmail.com>
2024-06-29 00:38:01 +0100

    (Linux) Try fix error

--- install.sh
+++ install.sh
@@ -9,25 +9,34 @@ python -m pip install --upgrade pip
 echo -e "${GREEN}Installing python pipenv...${NC}"
 python -m pip install pipenv
 
+python_app = ""
+
 if [ -f "/proc/version" ]; then
     echo -e "${GREEN}Installing python3.10-venv...${NC}"
     sudo apt install python3.10-venv --yes
+
+    echo -e "${GREEN}Creating python venv...${NC}"
+    python -m venv .venv
+
+    source .venv/bin/activate
+    python_app = "python"
+else
+    echo -e "${GREEN}Creating python venv...${NC}"
+    python -m venv .venv
+    python_app = "./.venv/Scripts/python.exe"
 fi
 
-echo -e "${GREEN}Creating python venv...${NC}"
-python -m venv .venv
-
 echo -e "${GREEN}Installing python pipenv...${NC}"
-"./.venv/Scripts/python.exe" -m pip install pipenv
+${python_app} -m pip install pipenv
 
 echo -e "${GREEN}Installing python GitPython...${NC}"
-"./.venv/Scripts/python.exe" -m pip install GitPython
+${python_app} -m pip install GitPython
 
 echo -e "${GREEN}Installing python termcolor...${NC}"
-"./.venv/Scripts/python.exe" -m pip install termcolor
+${python_app} -m pip install termcolor
 
 echo -e "${GREEN}Installing python django-admin...${NC}"
-"./.venv/Scripts/python.exe" -m pip install django-admin
+${python_app} -m pip install django-admin
 
 echo -e "${GREEN}Installing python django...${NC}"
-"./.venv/Scripts/python.exe" -m pip install django
+${python_app} -m pip install django
