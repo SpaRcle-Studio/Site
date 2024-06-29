9404a4b4715a8236100c6d54727ec886adbf578d
Monika0000 <rotaru5craft@gmail.com>
2024-06-29 03:37:42 +0100

    (Server) Fixed redirection.

--- SpaRcle/views.py
+++ SpaRcle/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    context = { }
-    return render(request, 'home.html', context)
 
+def home(request):
+    context = {}
+    return render(request, 'home.html', context)
